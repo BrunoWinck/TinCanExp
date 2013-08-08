45900877689d05a238de980bad9af65b32b2ca03
Bruno Winck, Kneaver <bwmgithub@kneaver.com>
2013-08-08 17:07:53 +0200

    Add Viewer

--- StatementViewer/resources/config.js
+++ StatementViewer/resources/config.js
@@ -3,7 +3,7 @@
 function Config() {
 	"use strict";
 }
-Config.endpoint = "https://jointventuregroup.waxlrs.com/TCAPI/";
+Config.endpoint = "https://jointventuregroup.waxlrs.com/TCAPI";
 Config.authUser = "QIIRadPjLX7eP7TX9RyD";
 Config.authPassword = "hP2sLO3a3Z0aA6fU3jlC";
 Config.actor = { "mbox":["<learner email>"], "name":["<Learner Name>"] };
