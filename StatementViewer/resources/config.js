082539cee2c7f1937aba57e070bd25b1c49d8e49
Bruno Winck, Kneaver <bwmgithub@kneaver.com>
2013-08-08 18:05:47 +0200

    Del config.js

--- StatementViewer/resources/config.js
+++
@@ -1,9 +0,0 @@
-//globals: equal, responseText, statement, ok, deepEqual, QUnit, module, asyncTest, Util, start, golfStatements, console
-/*jslint bitwise: true, browser: true, plusplus: true, maxerr: 50, indent: 4 */
-function Config() {
-	"use strict";
-}
-Config.endpoint = "https://jointventuregroup.waxlrs.com/TCAPI";
-Config.authUser = "QIIRadPjLX7eP7TX9RyD";
-Config.authPassword = "hP2sLO3a3Z0aA6fU3jlC";
-Config.actor = { "mbox":["<learner email>"], "name":["<Learner Name>"] };
