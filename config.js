fdd589993e1d2e2e34fe64948a661dd40fbee81b
Bruno Winck, Kneaver <bwmgithub@kneaver.com>
2013-08-08 17:02:47 +0200

    First success

---
+++ config.js
@@ -0,0 +1,9 @@
+//globals: equal, responseText, statement, ok, deepEqual, QUnit, module, asyncTest, Util, start, golfStatements, console
+/*jslint bitwise: true, browser: true, plusplus: true, maxerr: 50, indent: 4 */
+function Config() {
+	"use strict";
+}
+Config.endpoint = "https://jointventuregroup.waxlrs.com/TCAPI/statements/";
+Config.authUser = "QIIRadPjLX7eP7TX9RyD";
+Config.authPassword = "hP2sLO3a3Z0aA6fU3jlC";
+Config.actor = { "mbox":["<learner email>"], "name":["<Learner Name>"] };
--- web.js
+++ web.js
@@ -1,9 +1,93 @@
 var express = require('express');
 var fs = require('fs');
+var backbone = require('backbone');
+//var underscore = require('underscore');
+var csv = require('csv');
+var sys = require('util');
+var XMLHttpRequest = require("./XMLHttpRequest").XMLHttpRequest;
+
+
+var xhr = new XMLHttpRequest();
+
+var TinCan = require("./tincan");
+
 var infile = "index.html";
 var text1 = fs.readFileSync( infile);
 
-var app = express.createServer(express.logger());
+var Config = require('./config');
+
+TinCan.TinCan.DEBUG = true;
+/*
+var LRS = new TinCan.TinCan.LRS(
+                {
+                    endpoint: "https://jointventuregroup.waxlrs.com/TCAPI/", //Config.endpoint,
+                    username: Config.authUser,
+                    password: Config.authPassword
+//                    version: this.allVersions[i]
+                }
+		);
+*/		
+var tincanapi = new TinCan.TinCan;
+tincanapi.addRecordStore( 
+                {
+                    endpoint: "https://jointventuregroup.waxlrs.com/TCAPI/", //Config.endpoint,
+                    username: "QIIRadPjLX7eP7TX9RyD", // Config.authUser,
+                    password: "hP2sLO3a3Z0aA6fU3jlC", // Config.authPassword
+//                    version: this.allVersions[i]
+                }
+		);
+var statements =
+[ 
+   { 
+      "verb":{ 
+         "id":"http://example.com/verbs/sailed", 
+         "display":{ 
+            "en":"sailed" 
+         } 
+      }, 
+      "result":{ 
+         "completion":true, 
+         "success":true 
+      }, 
+      "context":{ 
+         "contextActivities":{ 
+            "other":{ 
+               "id":"http://example.com/activities/oceanrelated" 
+            } 
+         }, 
+         "instructor":{ 
+            "mbox":"mailto:sam@example.com", 
+            "name":"Sam" 
+         } 
+      }, 
+      "timestamp":"2011-05-25T20:34:05.787000+00:00", 
+      "object":{ 
+         "id":"http://example.com/activities/sailingcourse" 
+      }, 
+      "actor":{ 
+         "objectType":"Group", 
+         "member":[ 
+            { 
+               "mbox":"mailto:a@b.com", 
+               "name":"Bob" 
+            }, 
+            { 
+               "mbox":"mailto:b@c.com", 
+               "name":"Jones" 
+            } 
+         ], 
+         "name":"Team ABC" 
+      } 
+   }
+];
+
+tincanapi.sendStatements( statements, null);
+
+if (false)
+{
+
+var app = express(express.logger());
+//var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.setHeader('Content-Type', 'text/html');
@@ -14,3 +98,4 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+}
